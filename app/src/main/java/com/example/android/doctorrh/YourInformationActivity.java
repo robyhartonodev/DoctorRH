20c7a9b23f4633f8d1e7113ffa32ee9da06de6e4
Roby Hartono <roby.hartono@ges.thm.de>
2018-11-26 22:35:51 +0100

    Added New Activity YourInformationActivity

---
+++ app/src/main/java/com/example/android/doctorrh/YourInformationActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.doctorrh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YourInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_information);
+    }
+}
